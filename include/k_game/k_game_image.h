daec81cdb2277f1bbd754f45f56b8d63fa034521
szdxdxdx <3510745357@qq.com>
2025-03-13 16:25:03 +0800

    wip12

--- include/k_game/k_game_image.h
+++ include/k_game/k_game_image.h
@@ -3,7 +3,12 @@
 
 #include "./k_game_rect.h"
 
+/**
+ * TODO docs
+ */
 struct k_image;
+
+/* region [image_load] */
 
 struct k_image *k_image_load(const char *filepath);
 
@@ -11,10 +16,20 @@ int k_image_set_name(struct k_image *image, const char *image_name);
 
 struct k_image *k_image_find(const char *image_name);
 
+/* endregion */
+
+/* region [image_get] */
+
 int k_image_get_width(struct k_image *image);
 
 int k_image_get_height(struct k_image *image);
 
+/* endregion */
+
+/* region [image_draw] */
+
 int k_image_draw(struct k_image *image, const struct k_int_rect *src_rect, float dst_x, float dst_y);
+
+/* endregion */
 
 #endif
--- include/k_game/k_game_sound.h
+++ include/k_game/k_game_sound.h
@@ -3,6 +3,10 @@
 
 struct k_sound_BGM;
 
+struct k_sound_SFX;
+
+/* region [BGM_load] */
+
 struct k_sound_BGM *k_sound_BGM_load(const char *filepath);
 
 void k_sound_BGM_release(struct k_sound_BGM *sound);
@@ -10,6 +14,10 @@ void k_sound_BGM_release(struct k_sound_BGM *sound);
 int k_sound_BGM_set_name(struct k_sound_BGM *sound, const char *BGM_name);
 
 struct k_sound_BGM *k_sound_BGM_find(const char *BGM_name);
+
+/* endregion */
+
+/* region [BGM_play] */
 
 int k_sound_BGM_loop(struct k_sound_BGM *sound, int loops);
 
@@ -19,7 +27,9 @@ int k_sound_BGM_loop(struct k_sound_BGM *sound, int loops);
 
 // int k_sound_set_BGM_volume(float volume);
 
-struct k_sound_SFX;
+/* endregion */
+
+/* region [SFX_load] */
 
 struct k_sound_SFX *k_sound_SFX_load(const char *filepath);
 
@@ -29,8 +39,14 @@ int k_sound_SFX_set_name(struct k_sound_SFX *sound, const char *SFX_name);
 
 struct k_sound_SFX *k_sound_SFX_find(const char *SFX_name);
 
+/* endregion */
+
+/* region [SFX_play] */
+
 void k_sound_SFX_play(struct k_sound_SFX *sound);
 
 void k_sound_SFX_loop(struct k_sound_SFX *sound, int loops);
+
+/* endregion */
 
 #endif
