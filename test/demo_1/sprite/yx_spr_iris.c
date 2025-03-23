bfab80f14b543d9d59ff36d1327e5d68173e0cc7
szdxdxdx <3510745357@qq.com>
2025-03-23 15:57:42 +0800

    -

--- test/demo_1/sprite/yx_spr_iris.c
+++ test/demo_1/sprite/yx_spr_iris.c
@@ -3,105 +3,17 @@
 struct k_sprite *yx_spr_iris_gun = NULL;
 struct k_sprite *yx_spr_iris_bullet = NULL;
 
-static struct k_sprite *load_gun(const char *filepath, int frames_num, int delay) {
-
-    int scale = 2;
-
-    struct k_image *img = k_image_load(filepath);
-    int scaled_image_w = scale * k_image_get_width(img);
-    int scaled_image_h = scale * k_image_get_height(img);
-    struct k_image *img_player = k_image_scale(img, scaled_image_w, scaled_image_h);
-    k_image_release(img);
-
-    int sprite_w = k_image_get_width(img_player) / frames_num;
-    int sprite_h = k_image_get_height(img_player);
-
-    float origin_x = (float)scale * 15;
-    float origin_y = (float)scale * 23;
-
-    /* 素材中的动画都不超过 15 帧，此处不需要动态申请内存 */
-    struct k_sprite_frame_config frames_config[] = {
-            { img_player,  0 * sprite_w, 0, delay },
-            { img_player,  1 * sprite_w, 0, delay },
-            { img_player,  2 * sprite_w, 0, delay },
-            { img_player,  3 * sprite_w, 0, delay },
-            { img_player,  4 * sprite_w, 0, delay },
-            { img_player,  5 * sprite_w, 0, delay },
-            { img_player,  6 * sprite_w, 0, delay },
-            { img_player,  7 * sprite_w, 0, delay },
-            { img_player,  8 * sprite_w, 0, delay },
-            { img_player,  9 * sprite_w, 0, delay },
-            { img_player, 10 * sprite_w, 0, delay },
-            { img_player, 11 * sprite_w, 0, delay },
-            { img_player, 12 * sprite_w, 0, delay },
-            { img_player, 13 * sprite_w, 0, delay },
-            { img_player, 14 * sprite_w, 0, delay },
-            { img_player, 15 * sprite_w, 0, delay },
-    };
-
-    struct k_sprite_config config = {
-        .sprite_w   = sprite_w,
-        .sprite_h   = sprite_h,
-        .origin_x   = origin_x,
-        .origin_y   = origin_y,
-        .frames     = frames_config,
-        .frames_num = frames_num
-    };
-
-    return k_sprite_create(&config);
-}
-
-static struct k_sprite *load_bullet(const char *filepath, int frames_num, int delay) {
-
-    int scale = 2;
-
-    struct k_image *img = k_image_load(filepath);
-    int scaled_image_w = scale * k_image_get_width(img);
-    int scaled_image_h = scale * k_image_get_height(img);
-    struct k_image *img_player = k_image_scale(img, scaled_image_w, scaled_image_h);
-    k_image_release(img);
-
-    int sprite_w = k_image_get_width(img_player) / frames_num;
-    int sprite_h = k_image_get_height(img_player);
-
-    float origin_x = (float)sprite_w / 2;
-    float origin_y = (float)sprite_h / 2;
-
-    /* 素材中的动画都不超过 15 帧，此处不需要动态申请内存 */
-    struct k_sprite_frame_config frames_config[] = {
-            { img_player,  0 * sprite_w, 0, delay },
-            { img_player,  1 * sprite_w, 0, delay },
-            { img_player,  2 * sprite_w, 0, delay },
-            { img_player,  3 * sprite_w, 0, delay },
-            { img_player,  4 * sprite_w, 0, delay },
-            { img_player,  5 * sprite_w, 0, delay },
-            { img_player,  6 * sprite_w, 0, delay },
-            { img_player,  7 * sprite_w, 0, delay },
-            { img_player,  8 * sprite_w, 0, delay },
-            { img_player,  9 * sprite_w, 0, delay },
-            { img_player, 10 * sprite_w, 0, delay },
-            { img_player, 11 * sprite_w, 0, delay },
-            { img_player, 12 * sprite_w, 0, delay },
-            { img_player, 13 * sprite_w, 0, delay },
-            { img_player, 14 * sprite_w, 0, delay },
-            { img_player, 15 * sprite_w, 0, delay },
-    };
-
-    struct k_sprite_config config = {
-        .sprite_w   = sprite_w,
-        .sprite_h   = sprite_h,
-        .origin_x   = origin_x,
-        .origin_y   = origin_y,
-        .frames     = frames_config,
-        .frames_num = frames_num
-    };
-
-    return k_sprite_create(&config);
-}
-
 int yx_load_spr_iris(void) {
 
-    yx_spr_iris_gun = load_gun("./demo_1/sprite/iris/gun.png", 1, 32);
-    yx_spr_iris_bullet = load_bullet("./demo_1/sprite/iris/bullet.png", 1, 32);
+    struct yx_sprite_sheet sheet;
+
+    const char *config = "./demo_1/sprite/iris.json";
+    const char *image  = "./demo_1/sprite/iris.png";
+    yx_sprite_sheet_init(&sheet, image, 2.0f, config);
+
+    yx_spr_iris_gun = yx_sprite_load_from_sheet(&sheet, "gun", 15, 23);
+    yx_spr_iris_bullet  = yx_sprite_load_from_sheet(&sheet, "bullet", 16, 16);
+
+    yx_sprite_sheet_fini(&sheet);
     return 0;
 }
