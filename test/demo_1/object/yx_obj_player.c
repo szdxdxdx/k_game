38978f4652ec3bec691e4dba93a9d3e40a0ef831
szdxdxdx <3510745357@qq.com>
2025-04-10 00:38:31 +0800

    -

--- test/demo_1/object/yx_obj_player.c
+++ test/demo_1/object/yx_obj_player.c
@@ -218,7 +218,7 @@ struct k_object *yx_player_create(const struct yx_obj_player_config *config) {
     {
         struct yx_camera_target *target = yx_object_add_camera_follow(object, &player->x, &player->y);
         yx_camera_set_main_target(target);
-        yx_camera_target_set_weight(target, 3.5f);
+        yx_camera_target_set_weight(target, 3.0f);
     }
 
     return object;
