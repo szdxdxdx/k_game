1752b5e84ca98a9f9baf4e17ffd28859ed7432e8
szdxdxdx <3510745357@qq.com>
2025-04-20 13:00:59 +0800

    ------------

--- include/k_game/k_game/core/k_todo.h
+++ include/k_game/k_game/core/k_todo.h
@@ -10,9 +10,6 @@
 /* 若 `target` 被销毁，则 `object` 也跟着被销毁 */
 int k_object_set_follow_destruction(struct k_object *object, struct k_object *target);
 
-/* 设置 `object` 的销毁函数，当调用 `k_object_destroy()` 时，改为执行所指定的 `fn_destroy_callback` 函数 */
-void k_object_set_destroy_fn(struct k_object *object, void (*fn_destroy)(struct k_object *object));
-
 /* 遍历对象所挂载的组件，查找并返回指定组件类型的组件实例（有多个则返回找到的第一个） */
 struct k_component *k_object_find_component(struct k_object *object, struct k_component_type *component_type);
 
