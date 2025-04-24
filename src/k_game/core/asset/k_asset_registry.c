26b77c6b13495c158be62b795e663571f0d705eb
szdxdxdx <3510745357@qq.com>
2025-04-24 23:47:56 +0800

    -------------------------------------------------------------------------------------------------------

--- src/k_game/core/asset/k_asset_registry.c
+++ src/k_game/core/asset/k_asset_registry.c
@@ -1,6 +1,17 @@
+#include <string.h>
+
 #include "k_game/core/k_mem_alloc.h"
 
 #include "./k_asset_registry.h"
+
+static char *k__asset_registry_strdup(const char *str) {
+    char *copy = k__mem_alloc(strlen(str) + 1);
+    if (NULL == copy)
+        return NULL;
+
+    strcpy(copy, str);
+    return copy;
+}
 
 int k__asset_registry_init(struct k_asset_registry *registry) {
 
@@ -16,7 +27,7 @@ int k__asset_registry_init(struct k_asset_registry *registry) {
 
 void k__asset_registry_cleanup(struct k_asset_registry *registry, void (*fn_release)(struct k_asset_registry_node *registry_node)) {
 
-    if (NULL != registry) {
+    if (NULL != fn_release) {
         struct k_asset_registry_node *registry_node;
         struct k_list *asset_list = &registry->asset_list;
         struct k_list_node *iter, *next;
@@ -54,23 +65,30 @@ int k__asset_set_name(struct k_asset_registry *registry, struct k_asset_registry
 
         if ('\0' != map_node->key[0]) {
             k_str_intrusive_map_del(map_node);
+            k__mem_free((void *)map_node->key);
             map_node->key = "";
         }
+
+        return 0;
     }
     else {
         if (NULL != k_str_intrusive_map_get(&registry->name_map, asset_name)) {
             return -1; /* TODO log("同名资源已存在") */
         }
 
+        char *name_copy = k__asset_registry_strdup(asset_name);
+        if (NULL == name_copy)
+            return -1;
+
         if ('\0' != map_node->key[0]) {
             k_str_intrusive_map_del(map_node);
+            k__mem_free((void *)map_node->key);
         }
-        else {
-            k_str_intrusive_map_add_directly(&registry->name_map, asset_name, map_node);
-        }
-    }
 
-    return 0;
+        k_str_intrusive_map_add_directly(&registry->name_map, name_copy, map_node);
+
+        return 0;
+    }
 }
 
 const char *k__asset_get_name(struct k_asset_registry_node *registry_node) {
